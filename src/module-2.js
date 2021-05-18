5445564b1b888de9839ef83aff59d47980daa181
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-05-19 02:06:46 +0300

    solve task 1

--- src/module-2.js
+++ src/module-2.js
@@ -1,4 +1,11 @@
 // ========== TASK 1 ==========
+// const checkAge = function (age) {
+//   return age >= 18 ? 'You are an adult' : 'You are a minor';
+// };
+
+// console.log(checkAge(18));
+// console.log(checkAge(15));
+// console.log(checkAge(28));
 
 // ========== TASK 2 ==========
 
