c1c3c4bd97729a366cb27ce5bc41a8570dca7ab5
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-05-19 14:10:04 +0300

    solve tasks to 11

--- src/module-2.js
+++ src/module-2.js
@@ -8,9 +8,102 @@
 // console.log(checkAge(28));
 
 // ========== TASK 2 ==========
+// const checkPassword = function (password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+
+//   return password === ADMIN_PASSWORD
+//     ? 'Welcome!'
+//     : 'Access denied, wrong password!';
+// };
+
+// console.log(checkPassword('jqueryismyjam'));
 
 // ========== TASK 3 ==========
+// const checkStorage = function (available, ordered) {
+// ========= VAR 1 ==========
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+// ========= VAR 2 ==========
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   return ordered > available
+//     ? 'Your order is too large, not enough goods in stock!'
+//     : 'The order is accepted, our manager will contact you';
+// };
+
+// console.log(checkStorage(100, 0));
 
 // ========== TASK 4 ==========
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// console.table(fruits);
 
 // ========== TASK 5 ==========
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// console.table(fruits);
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(lastElement);
+
+// ========== TASK 6 ==========
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// fruits[1] = 'peach';
+// fruits[3] = 'banana';
+
+// console.log(fruits);
+
+// ========== TASK 7 ==========
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const fruitsArrayLength = fruits.length;
+
+// console.log(fruitsArrayLength);
+
+// ========== TASK 8 ==========
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElementIndex);
+// console.log(lastElement);
+
+// ========== TASK 9 ==========
+// function getExtremeElements(array) {
+//   return [array[0], array[array.length - 1]];
+// }
+
+// console.log(getExtremeElements([2, 5, 56, 'number']));
+// console.log(getExtremeElements([100, 5, 56, 1000]));
+
+// ========== TASK 10 ==========
+// function splitMessage(message, delimeter) {
+//   let words;
+//   words = message.split(delimeter);
+//   return words;
+// }
+
+// const string = 'London is the capital of Great Britain';
+// console.log(splitMessage(string, ' '));
+// console.log(splitMessage('Bond', ''));
+
+// ========== TASK 11 ==========
+
+// ========== TASK 12 ==========
+
+// ========== TASK 13 ==========
+
+// ========== TASK 14 ==========
+
+// ========== TASK 15 ==========
+
+// ========== TASK 16 ==========
