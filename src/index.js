9d299674fdffd64d5c4d90a0436a24cfe5c30484
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-06-07 23:10:26 +0300

    solved all tasks of module 4

--- src/index.js
+++ src/index.js
@@ -189,43 +189,35 @@
 // console.log(burgerShackMessage);
 
 //========== TASK 10 ==========
-const service = {
-  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
-  subscribe(email) {
-    this.mailingList.push(email);
-    return `Почта ${email} добавлена в рассылку.`;
-  },
-  unsubscribe(email) {
-    this.mailingList = this.mailingList.filter(e => e !== email);
-    return `Почта ${email} удалена из рассылки.`;
-  },
-};
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter(e => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
 
-function logAndInvokeAction(email, action) {
-  console.log(`Выполняем действие с ${email}.`);
-  return action(email);
-}
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
 
-const firstInvoke = logAndInvokeAction.bind(
-  service,
-  'kiwi@mail.uk',
-  service.subscribe.bind(service, 'kiwi@mail.uk'),
-);
+// const firstInvoke = logAndInvokeAction(
+//   'kiwi@mail.uk',
+//   service.subscribe.bind(service),
+// );
 
-console.log(firstInvoke());
-// Почта kiwi@mail.uk добавлена в рассылку.
+// console.log(firstInvoke);
 
-console.log(service.mailingList);
-/* ['mango@mail.com', 
-    'poly@hotmail.de', 
-    'ajax@jmail.net', 
-    'kiwi@mail.uk']*/
-const secondInvoke = logAndInvokeAction.bind(
-  service,
-  'poly@hotmail.de',
-  service.unsubscribe.bind(service, 'poly@hotmail.de'),
-);
-console.log(secondInvoke());
-// Почта poly@hotmail.de удалена из рассылки.
+// console.log(service.mailingList);
+// const secondInvoke = logAndInvokeAction(
+//   'poly@hotmail.de',
+//   service.unsubscribe.bind(service),
+// );
+// console.log(secondInvoke);
 
-console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+// console.log(service.mailingList);
